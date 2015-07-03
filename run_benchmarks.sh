599fe0df501a6cf1da2a14f49d50b7e3b266ac71
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2015-07-04 01:00:52 +0200

    Added range bench

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -24,16 +24,28 @@ do
 	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/sync.tl.$proc.log"
 	rm bench.log
 
-	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l LockFreeSkipList -t $proc -r 4000 -d 10"
+	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l LockFreeSkipList -t $proc -r 10000 -d 10"
 	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/test.lfs.$proc.log"
 	rm bench.log
 
-	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l LazySkipList -t $proc -r 4000 -d 10"
+	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l LazySkipList -t $proc -r 10000 -d 10"
 	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/test.ls.$proc.log"
 	rm bench.log
 
-	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l TestList -t $proc -r 4000 -d 10"
+	./benchmark.sh -n $repeats -c "./bin/test_correct_simple -l TestList -t $proc -r 10000 -d 10"
 	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/test.tl.$proc.log"
+	rm bench.log
+
+	./benchmark.sh -n $repeats -c "./bin/test_correct_range -l LockFreeSkipList -t $proc -r 10000 -d 10"
+	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/range.lfs.$proc.log"
+	rm bench.log
+
+	./benchmark.sh -n $repeats -c "./bin/test_correct_range -l LazySkipList -t $proc -r 10000 -d 10"
+	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/range.ls.$proc.log"
+	rm bench.log
+
+	./benchmark.sh -n $repeats -c "./bin/test_correct_range -l TestList -t $proc -r 10000 -d 10"
+	grep "#bench" bench.log | sed 's/\#bench\#//g'  > "results/range.tl.$proc.log"
 	rm bench.log
 done
 
